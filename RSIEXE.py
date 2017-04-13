23b5d1eed12e9e972b9a74c030d2de71e2fbe919
LauriHolopainen <holopail@student.tut.fi>
2017-04-13 15:44:48 +0300

    Fixed lot of bugs and tested for correct values

--- RSIEXE.py
+++ RSIEXE.py
@@ -9,15 +9,26 @@ import pandas_datareader as web
 def RSI(data):
     AVG_gain = 0
     AVG_loss = 0
-
+    i = 0
+    close_prices = []
     for info in data.values:
-        open = float(info[0])
         close = float(info[3])
-        gainloss = close-open
+        close_prices.append(close)
+
+    for i in range(0, len(close_prices)-1):
+        close1 = close_prices[i]
+        close2 = close_prices[i+1]
+
+        gainloss = close2 - close1
+
         if gainloss > 0:
             AVG_gain += gainloss
         elif gainloss < 0:
             AVG_loss -= gainloss
+
+
+    #print("AVG GAIN " + str(AVG_gain/i))
+    #print("AVG LOSS " + str(AVG_loss/i))
 
     RS = AVG_gain/AVG_loss
     RSI = 100 - (100/(1+RS))
@@ -29,7 +40,6 @@ def weekendEliminator(end, timeframe):
     i = 0
     while i < timeframe:
         end -= timedelta(days=1)
-        print(end)
         if end.weekday() > 4:
             continue
 
@@ -46,7 +56,7 @@ def main():
     start = datetime.today()
     RSIlist = []
     dates = []
-    i = 0
+    i = 2
 
     #Creates recent business days according to timeframe and adds them to list
     while i < timeframe:
@@ -55,26 +65,32 @@ def main():
             continue
 
         else:
+            print(start)
             dates.append(start)
             i += 1
-
-
+    print(len(dates))
+    #gets the required course history
     try:
-        df = web.DataReader(symbol, "yahoo",weekendEliminator(end, 2*timeframe), end)
+        df = web.DataReader(symbol, "yahoo",weekendEliminator(end, 2*timeframe+1), end)
 
     except:
         try:
-            df = web.DataReader(symbol, "google", weekendEliminator(end, 2*timeframe), end)
+            df = web.DataReader(symbol, "google", weekendEliminator(end, 2*timeframe+1), end)
         except:
-            df = web.DataReader(symbol, "fred", weekendEliminator(end, 2*timeframe), end)
-
+            try:
+                df = web.DataReader(symbol, "fred", weekendEliminator(end, 2*timeframe+1), end)
+            except:
+                print("Course history couldn't been found for chosen stock. Please check for spelling.")
+                main()
 
     #counts RSI for specific day and adds it to RSIlist
-    for i in range(0, timeframe):
-        ending = end - timedelta(days=timeframe-i)
-        start = weekendEliminator(ending, timeframe)
+    for i in range(2, timeframe):
+        ending = weekendEliminator(end, i)
+        start = weekendEliminator(ending, timeframe + 1)
+        print(str(start) + "    " + str(ending))
         period = df[start:ending]
         RSIlist.append(RSI(period))
+        print(RSI(period))
     plt.plot(dates, RSIlist)
     plt.show()
 
